a27af817bd848105c902b8c4c139634c17f4074e
divijesh <divijeshvarma@pm.me>
2021-02-17 11:35:48 +0530

    if example

--- ifExample.py
+++ ifExample.py
@@ -4,3 +4,21 @@ if name == 'divi':
     print('hi divi')
 
 print('done')
+
+# if else
+password = 'divi'
+if password == 'jesh':
+    print('Access granted')
+else:
+    print('Access denied')
+
+# elif
+name = 'divi'
+age = 35
+
+if age == 20:
+    print('hi young divi')
+elif age > 20:
+    print('hi original divi')
+else:
+    print('hi old divi')
