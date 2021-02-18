c5ca064cdee237ef6da6c7071a88fb33d43c4e30
divijesh <divijeshvarma@pm.me>
2021-02-18 09:42:43 +0530

    for example

---
+++ forExample.py
@@ -0,0 +1,21 @@
+
+for i in range(5):
+    print(f'divi {i}')
+
+for i in range(10, 15):
+    print(f'jesh {i}')
+
+for i in range(0, 6, 2):
+    print(f'varma {i}')
+
+for i in range(6, -1, -1):
+    print(f'varma {i}')
+
+##########
+
+total = 0
+
+for num in range(101):
+    total = total + num
+
+print(total)
