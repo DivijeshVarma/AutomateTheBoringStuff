bb44c86985339ed2c582e8aed5e585e0c76675d1
divijesh <divijeshvarma@pm.me>
2021-02-18 10:55:32 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,30 @@
+def hello():
+    print('hello')
+    print('universe')
+
+
+hello()
+hello()
+
+
+#############
+def hi(name):
+    print(f"hello {name}")
+
+
+hi('divi')
+hi('varma')
+
+
+##############
+def plusone(num):
+    return num + 1
+
+
+sum = plusone(5)
+print(sum)
+
+print('hello', end='')
+print('world')
+
+print('cat', 'dog', 'mouse', sep='-')
