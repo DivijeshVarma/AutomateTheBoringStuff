744a89d98670ad9c7c10583bb4523873b518a619
divijesh <divijeshvarma@pm.me>
2021-02-25 15:29:34 +0530

    regex phonenum

--- phonenumRE.py
+++ phonenumRE.py
@@ -1,8 +1,13 @@
 import re
 
 # pass raw strings to compile function, text for pattern looking for
-phonenumregex = re.compile(r"\d\d\d\d\d\d\d\d\d\d")
+phonenumregex = re.compile(r"(\d\d\d)(\d\d\d\d\d\d\d)")
 mo = phonenumregex.search('call me at 9493675436 or another 9495767239')
+print(mo.group(1))
+print(mo.group(2))
+
+phonenumregex = re.compile(r"\(\d\d\d\) \d\d\d\d\d\d\d")
+mo = phonenumregex.search('call me at (949) 3675436')
 print(mo.group())
 
-print(phonenumregex.findall('call me at 9493675436 or another 9495767239'))
+# print(phonenumregex.findall('call me at 9493675436 or another 9495767239'))
